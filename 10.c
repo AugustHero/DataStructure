2ccaf6e6b2711385af627e211b8601f911268f8d
zhou199604 <94295197+AugustHero@users.noreply.github.com>
2022-08-06 20:43:26 +0800

    11

---
+++ 10.c
@@ -0,0 +1,5 @@
+// 10、假定数组中有多个零元素，先要将数组中所有非零元素移到数组的前端。
+// 请用你学过的高级语言，给出实现上述要求的程序，建议用函数实现。
+
+//https://blog.csdn.net/cnds123321/article/details/106291801/
+//https://blog.csdn.net/weuia/article/details/108858974
